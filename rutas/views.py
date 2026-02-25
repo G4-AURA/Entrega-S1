0d140c56daf01fa9c8bc4be24be7ddf2f0eba9bc
STX3837 <albertopaleteirolopez@gmail.com>
2026-02-25 13:23:10 +0100

    feat: correcciones codigo

--- rutas/views.py
+++ rutas/views.py
@@ -1,27 +1,51 @@
+from django.db.models import Prefetch
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET
 
-from .models import Ruta
+from .models import Ruta, Parada
 
+MAX_RUTAS_PAGE_SIZE = 100
 
 @require_GET
 def rutas_catalogo(request):
+    try:
+        limit = int(request.GET.get("limit", MAX_RUTAS_PAGE_SIZE))
+    
+    except (TypeError, ValueError):
+        limit = MAX_RUTAS_PAGE_SIZE
+    
+    if limit <= 0:
+        limit = MAX_RUTAS_PAGE_SIZE
+    
+    elif limit > MAX_RUTAS_PAGE_SIZE:
+        limit = MAX_RUTAS_PAGE_SIZE
+    
+    try:
+        offset = int(request.GET.get("offset", 0))
+    
+    except (TypeError, ValueError):
+        offset = 0
+    
+    if offset < 0:
+        offset = 0
+    
     rutas = (
         Ruta.objects.select_related("guia", "guia__user", "guia__user__user")
-        .prefetch_related("paradas")
+        .prefetch_related(
+            Prefetch('paradas', queryset=Parada.objects.order_by('orden'))
+        )
         .order_by("id")
-    )
+    )[offset:offset + limit]
 
     data = []
     for ruta in rutas:
         guia_username = None
-        guia_id = None
+        guia_id = ruta.guia_id
         if ruta.guia and ruta.guia.user and ruta.guia.user.user:
             guia_username = ruta.guia.user.user.username
-            guia_id = ruta.guia.id
 
         paradas_data = []
-        for parada in ruta.paradas.all().order_by("orden"):
+        for parada in ruta.paradas.all():
             coords = None
             if parada.coordenadas:
                 coords = {
