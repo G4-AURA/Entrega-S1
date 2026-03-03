540039d865507b372cd5fed31b6eb0be2293a617
felpennun <felpennun@alum.us.es>
2026-03-03 20:31:55 +0100

    fix/form-new-users

--- config/forms.py
+++ config/forms.py
@@ -7,17 +7,6 @@ class RegistroUsuarioForm(UserCreationForm):
     FORMULARIO PARA REGISTRAR USUARIOS
     '''
     # Definimos las opciones para el tipo de cuenta
-    OPCIONES_CUENTA = [
-        ('guia', 'Guía Turístico'),
-        ('turista', 'Turista'),
-    ]
-    
-    tipo_cuenta = forms.ChoiceField(
-        choices=OPCIONES_CUENTA, 
-        widget=forms.RadioSelect,
-        label="¿Qué tipo de cuenta quieres crear?"
-    )
-
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields + ('email', 'first_name', 'last_name') # OPCIONALES
