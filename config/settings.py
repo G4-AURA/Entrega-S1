eec94f4daee656496e203e7a33561e17f1c1711d
marioreyesapresa <marreyapr@alum.us.es>
2026-03-10 12:40:30 +0100

    fix: Arreglo login

--- config/settings.py
+++ config/settings.py
@@ -156,7 +156,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # --- LOGIN / LOGOUT ---
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/catalogo/'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 
