439e3a94cf15460012ab0edc349e7ae737dc6345
CurroAyala <curroayala18@gmail.com>
2026-03-21 18:00:10 +0100

    fix: redirigir al landing page al cerrar sesión

--- config/settings.py
+++ config/settings.py
@@ -199,7 +199,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # --- LOGIN / LOGOUT ---
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/accounts/login/'
+LOGOUT_REDIRECT_URL = '/'
 
 
 # --- API KEYS ---
