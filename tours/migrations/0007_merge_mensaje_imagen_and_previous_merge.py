399d6705029fa9eb6a8e223212d2521ddb152474
javramnun <javramnun@alum.us.es>
2026-03-22 18:57:57 +0100

    fix

---
+++ tours/migrations/0007_merge_mensaje_imagen_and_previous_merge.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("tours", "0005_mensajechat_imagen"),
+        ("tours", "0006_merge_20260322_1844"),
+    ]
+
+    operations = []
