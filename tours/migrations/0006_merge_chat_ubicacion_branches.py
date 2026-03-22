7176b5d16c9e3f711cbb4895a7f116024773f561
javramnun <javramnun@alum.us.es>
2026-03-22 18:37:54 +0100

    fix error test

---
+++ tours/migrations/0006_merge_chat_ubicacion_branches.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("tours", "0004_ubicacionvivo_turista_nullable_usuario"),
+        ("tours", "0005_repair_mensaje_chat_remitente_nullable"),
+    ]
+
+    operations = []
