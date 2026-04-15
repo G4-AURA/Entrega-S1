f404ccc8a8fbed031bd109da264aff00f5782770
max <cortimax03@gmail.com>
2026-04-15 05:21:07 +0200

    fix(deploy): configure SSL for Celery Redis in worker startup

--- start_celery_cloudrun.sh
+++ start_celery_cloudrun.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-export CELERY_BROKER_URL="${CELERY_BROKER_URL}?ssl_cert_reqs=CERT_NONE"
-
 python -m http.server ${PORT:-8080} &
+export CELERY_BROKER_USE_SSL='{"ssl_cert_reqs": 0}'
+export CELERY_REDIS_BACKEND_USE_SSL='{"ssl_cert_reqs": 0}'
 celery -A config worker --loglevel=info
