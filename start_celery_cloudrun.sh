64e1e6580bda7c9b8f0145f3a01e0331bbcc13bb
max <cortimax03@gmail.com>
2026-04-15 04:02:03 +0200

    fix(deploy): set Celery broker URL via env for SSL Redis

--- start_celery_cloudrun.sh
+++ start_celery_cloudrun.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+export CELERY_BROKER_URL="${CELERY_BROKER_URL}?ssl_cert_reqs=CERT_NONE"
+
 python -m http.server ${PORT:-8080} &
-celery -A config worker --loglevel=info \
-  --broker="$CELERY_BROKER_URL?ssl_cert_reqs=CERT_NONE"
+celery -A config worker --loglevel=info
