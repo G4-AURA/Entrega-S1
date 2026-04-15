167468062053e86aca4ad29c9a19d13eb5a1aecd
max <cortimax03@gmail.com>
2026-04-15 03:41:50 +0200

    fix(deploy): add SSL options for Celery Redis on Cloud Run

--- start_celery_cloudrun.sh
+++ start_celery_cloudrun.sh
@@ -2,4 +2,5 @@
 set -e
 
 python -m http.server ${PORT:-8080} &
-celery -A config worker --loglevel=info
+celery -A config worker --loglevel=info \
+  --broker="$CELERY_BROKER_URL?ssl_cert_reqs=CERT_NONE"
